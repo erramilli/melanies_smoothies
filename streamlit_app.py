9e1529c21d60991d9763ed8f001d0cd245624ce3
erramilli <erramilli@hotmail.com>
2025-07-20 10:09:26 -0500

    Update streamlit_app.py
    moved the nutrition info to fruitschosen block

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 # Write directly to the app
@@ -22,7 +23,9 @@ if ingredients_list:
     #st.text(ingredients_list)
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
-       ingredients_string += fruit_chosen
+        ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
@@ -32,7 +35,5 @@ if ingredients_list:
     if time_to_insert:
       session.sql(my_insert_stmt).collect()
       st.success('Your Smoothie is ordered! ' + name_on_order)
-import requests
-smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
+
